6de3f604a8afc72dcc16ec5a93a08bf08a9f3c82
Sakthi Murugan <sakthimurugan@Sakthis-MacBook-Air.local>
2026-06-27 19:38:59 +0530

    Fix generated short URL endpoint

--- app/main.py
+++ app/main.py
@@ -27,17 +27,6 @@ from fastapi.responses import RedirectResponse
 from app.database import SessionLocal
 from app.models import URL
 
-@app.get("/{short_code}")
-def redirect(short_code: str):
-    db = SessionLocal()
-
-    url = db.query(URL).filter(URL.short_code == short_code).first()
-
-    if not url:
-        return {"detail": "Not Found"}
-
-    return RedirectResponse(url.long_url)
-
 @app.websocket("/ws")
 async def websocket(ws: WebSocket):
     await ws.accept()
